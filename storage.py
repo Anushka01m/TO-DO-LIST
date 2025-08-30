6bc40d3f857c2ce507f6973e56b7f1be929ddb0e
Anushka Maitra <anushka.mukti3011@gmail.com>
2025-08-30 22:20:35 +0530

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,1 +1,19 @@
+# storage.py
+
+import json
+from task import Task
+
+FILE = "tasks.json"
+
+def load_tasks():
+    try:
+        with open(FILE, "r") as f:
+            data = json.load(f)
+            return [Task.from_dict(d) for d in data]
+    except FileNotFoundError:
+        return []
+
+def save_tasks(tasks):
+    with open(FILE, "w") as f:
+        json.dump([t.to_dict() for t in tasks], f, indent=2)
 
