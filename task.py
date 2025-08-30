e71506e6e4e28b0e43c57cc819cdae85cec8e3d7
Anushka Maitra <anushka.mukti3011@gmail.com>
2025-08-30 22:20:05 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,1 +1,19 @@
+# task.py
+
+class Task:
+    def __init__(self, title, due_date=None, priority="Medium"):
+        self.title = title
+        self.due_date = due_date
+        self.priority = priority
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "due_date": self.due_date,
+            "priority": self.priority
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Task(data["title"], data.get("due_date"), data.get("priority"))
 
